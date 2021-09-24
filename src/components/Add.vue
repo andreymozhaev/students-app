c1edb8385ee79518fc81f800994f808ef8a6db93
Andrey <dedok-iu3@yandex.ru>
2021-09-24 13:35:35 +0300

    camera adaptive

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,58 +1,53 @@
 <template>
   <b-container>
     <b-row>
-      <b-col>
+      <b-col md="4" offset-md="4">
         <b-input
           placeholder="Введите имя и фамилию"
           v-model="student"
         ></b-input>
       </b-col>
-      <b-col>
+      <b-col v-if="false">
         <b-form-file @change="previewImage" accept="image/*"></b-form-file>
       </b-col>
-      <b-col>
+      <b-col v-if="false">
         <b-button variant="danger" @click="onUpload" v-if="!progress"
           >Зарегистрироваться</b-button
         >
         <b-spinner variant="primary" v-if="progress"></b-spinner>
       </b-col>
     </b-row>
-    <b-row>
-      <b-col></b-col>
-      <b-col class="text-center">
+    <b-row v-if = "!student.trim() == ''">
+      <b-col class="text-center" md="4" offset-md="4">
         <b-button variant="primary" class="m-5" @click="toggleCamera">
           <span v-if="!isCameraOpen">Открыть камеру</span>
           <span v-else>Закрыть камеру</span>
         </b-button>
       </b-col>
-      <b-col></b-col>
     </b-row>
-    <b-row>
-      <b-col></b-col>
-      <b-col class="text-center">
+    <b-row v-if = "!student.trim() == ''">
+      <b-col class="text-center" md="4" offset-md="4">
         <b-spinner
           variant="primary"
           v-show="isCameraOpen && isLoading"
         ></b-spinner>
       </b-col>
-      <b-col></b-col>
     </b-row>
 
     <b-row
-      v-if="isCameraOpen"
+      v-if="isCameraOpen && !student.trim() == ''"
       v-show="!isLoading"
       class="camera-box"
       :class="{ flash: isShotPhoto }"
     >
-      <b-col></b-col>
-      <b-col>
+      <b-col md="4" offset-md="4">
         <div class="camera-shutter" :class="{ flash: isShotPhoto }"></div>
 
         <video
+          id="camera"
           v-show="!isPhotoTaken"
           ref="camera"
-          :width="450"
-          :height="337.5"
+          width="100%"
           autoplay
         ></video>
 
@@ -60,14 +55,11 @@
           v-show="isPhotoTaken"
           id="photoTaken"
           ref="canvas"
-          :width="450"
-          :height="337.5"
         ></canvas>
       </b-col>
-      <b-col></b-col>
     </b-row>
 
-    <b-row>
+    <b-row v-if = "!student.trim() == ''">
       <b-col></b-col>
       <b-col class="text-center">
         <b-button
@@ -83,7 +75,7 @@
       <b-col></b-col>
     </b-row>
 
-    <b-row v-if="isPhotoTaken && isCameraOpen" class="camera-download">
+    <b-row v-if="isPhotoTaken && isCameraOpen && !student.trim() == ''" class="camera-download">
       <b-col></b-col>
       <b-col class="text-center">
         <b-button variant="success" id="downloadPhoto" @click="downloadImage" v-if="!progress">
@@ -122,7 +114,7 @@ export default {
       isPhotoTaken: false,
       isShotPhoto: false,
       isLoading: false,
-      link: "#",
+      link: "#"
     };
   },
   methods: {
@@ -200,6 +192,9 @@ export default {
     },
 
     takePhoto() {
+      const h = this.$refs.camera.videoHeight;
+      const w = this.$refs.camera.videoWidth;
+      const ratio = h / w;
       if (!this.isPhotoTaken) {
         this.isShotPhoto = true;
 
@@ -213,7 +208,8 @@ export default {
       this.isPhotoTaken = !this.isPhotoTaken;
 
       const context = this.$refs.canvas.getContext("2d");
-      context.drawImage(this.$refs.camera, 0, 0, 450, 337.5);
+      this.$refs.canvas.height = this.$refs.canvas.width * ratio;
+      context.drawImage(this.$refs.camera, 0, 0, this.$refs.canvas.width, this.$refs.canvas.height);
     },
 
     async downloadImage() {
@@ -247,9 +243,13 @@ export default {
       this.$router.push({ name: "home" });
     },
   },
-  created() {},
+  created() {
+  },
 };
 </script>
 
 <style>
+#photoTaken{
+  width: 100%;
+}
 </style>
